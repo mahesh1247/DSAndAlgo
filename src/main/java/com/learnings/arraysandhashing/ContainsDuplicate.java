ea666d914ecf853ec311020340c5cded59d6a785
Myneni Mahesh <myneni.nani@gmail.com>
2024-07-23 21:34:23 +0530

    First Program
    First Program

---
+++ src/main/java/com/learnings/arraysandhashing/ContainsDuplicate.java
@@ -0,0 +1,22 @@
+package com.learnings.arraysandhashing;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ContainsDuplicate {
+    public static void main(String[] args) {
+        ContainsDuplicate containsDuplicate = new ContainsDuplicate();
+        System.out.print(containsDuplicate.containsDuplicate(new int[]{1,2,3,4,5,1}));
+    }
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> elementsStore = new HashSet<>();
+        for(int i : nums){
+            if(elementsStore.contains(i)){
+                return true;
+            } else {
+                elementsStore.add(i);
+            }
+        }
+        return false;
+    }
+}
